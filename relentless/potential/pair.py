b216215b2cf06ec3ac1df33ee89765b1d81d7e7d
adisr04 <adithya.sr@utexas.edu>
2020-07-16 13:43:47 -0500

    Add features, documentation, unit tests

--- relentless/potential/pair.py
+++ relentless/potential/pair.py
@@ -156,32 +156,151 @@ class LennardJones(PairPotential):
             d = d.item()
         return d
 
-""" TODO: these potentials need updating
 class Yukawa(PairPotential):
+    """Yukawa pair potential.
+
+    .. math::
+
+    u(r) = \varepsilon\frac{{e}^{{-\kappa{r}}}}{{r}}
+
+    The required coefficients per pair are:
+
+    - :math:`\varepsilon`: interaction energy scale
+    - :math:`\kappa`: screening length inverse
+
+    The optional coefficients per pair are:
+
+    - ``rmin``: minimum radius, energy and force are 0 for ``r < rmin``. Ignored if ``False`` (default).
+    - ``rmax``: maximum radius, energy and force are 0 for ``r > rmax`` Ignored if ``False`` (default).
+    - ``shift``: If ``True``, shift potential to zero at ``rmax`` (default is ``False``).
+
+    Parameters
+    ----------
+    types : array_like
+        List of types (A type must be a `str`).
+
+    """
     def __init__(self, types, shift=False):
         super().__init__(types=types, params=('epsilon','kappa'))
 
-    def energy(self, r, epsilon, kappa, **params):
-        r,u = self._zeros(r)
+    def _energy(self, r, epsilon, kappa, **params):
+        """Evaluates the Yukawa potential energy.
+
+        Parameters
+        ----------
+        r : array_like
+            The value or values of r at which to evaluate the energy.
+        epsilon : float or int
+            The epsilon parameter for the potential function.
+        kappa : float or int
+            The kappa parameter for the potential function.
+
+        Returns
+        -------
+        array_like
+            Returns the value or values of the energy evaluated at r.
+
+        Raises
+        ------
+        ValueError
+            If kappa is not a positive number.
+
+        """
+        if kappa < 0:
+            raise ValueError('kappa must be positive')
+        r,u,s = self._zeros(r)
         flags = ~np.isclose(r,0)
 
+        # evaluate potential
         u[flags] = epsilon*np.exp(-kappa*r[flags])/r[flags]
         u[~flags] = np.inf
 
+        if s:
+            u = u.item()
         return u
 
-    def force(self, r, pair):
-        # load parameters for the pair
-        params = self.coeff.evaluate(pair)
-        epsilon = params['epsilon']
-        kappa = params['kappa']
+    def _force(self, r, epsilon, kappa, **params):
+        """Evaluates the Yukawa force.
 
-        r,f = self._zeros(r)
+        Parameters
+        ----------
+        r : array_like
+            The value or values of r at which to evaluate the force.
+        epsilon : float or int
+            The epsilon parameter for the potential function.
+        kappa : float or int
+            The kappa parameter for the potential function.
+
+        Returns
+        -------
+        array_like
+            Returns the value or values of the force evaluated at r.
+
+        Raises
+        ------
+        ValueError
+            If kappa is not a positive number.
+
+        """
+        if kappa < 0:
+            raise ValueError('kappa must be positive')
+        r,f,s = self._zeros(r)
         flags = ~np.isclose(r,0)
 
+        # evaluate force
         f[flags] = epsilon*np.exp(-kappa*r[flags])*(1.+kappa*r[flags])/r[flags]**2
         f[~flags] = np.inf
 
+        if s:
+            f = f.item()
+        return f
+
+    def _derivative(self, param, r, epsilon, kappa, **params):
+        """Evaluates the Yukawa parameter derivative.
+
+        Parameters
+        ----------
+        param : `str`
+            The parameter with respect to which to take the derivative.
+        r : array_like
+            The value or values of r at which to evaluate the derivative.
+        epsilon : float or int
+            The epsilon parameter for the potential function.
+        kappa : float or int
+            The kappa parameter for the potential function.
+
+        Returns
+        -------
+        array_like
+            Returns the value or values of the derivative evaluated at r.
+
+        Raises
+        ------
+        ValueError
+            If kappa is not a positive number.
+        ValueError
+            If the parameter with respect to which to take the derivative is not 'kappa' or 'epsilon'.
+
+        """
+        if kappa < 0:
+            raise ValueError('kappa must be positive')
+        r,d,s = self._zeros(r)
+        flags = ~np.isclose(r,0)
+
+        # evaluate derivative
+        if param == 'epsilon':
+            d[flags] = np.exp(-kappa*r[flags])/r[flags]
+            d[~flags] = np.inf
+        elif param == 'kappa':
+            d = -epsilon*np.exp(-kappa*r)
+        else:
+            raise ValueError('The Yukawa parameters are kappa and epsilon.')
+
+        if s:
+            d = d.item()
+        return d
+
+"""TODO: these potentials need updating
 class AkimaSpline(PairPotential):
     def __init__(self, types, num_knots, shift=False):
         assert num_knots >= 2, "Must have at least 2 knots"
