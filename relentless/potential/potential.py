Fix parameter derivative calculation and tests

relentless/potential/potential.py
@@ -307,7 +307,7 @@ class PairPotential(abc.ABC):
         ValueError
             If any value in `r` is negative.
         ValueError
-            If the potential is to be shifted without setting `rmax`.
+            If the potential is shifted without setting `rmax`.
 
         """
         params = self.coeff.evaluate(pair)
@@ -413,6 +413,8 @@ class PairPotential(abc.ABC):
         TypeError
             If the parameter with respect to which to take the derivative
             is not a :py:class:`Variable`.
+        ValueError
+            If the potential is shifted without setting `rmax`.
 
         """
         params = self.coeff.evaluate(pair)
@@ -446,7 +448,7 @@ class PairPotential(abc.ABC):
             if p=='rmin':
                 # rmin deriv
                 flags = r < params['rmin']
-                deriv[flags] = -self._force(params['rmin'], **params)*dp_dvar
+                deriv[flags] += -self._force(params['rmin'], **params)*dp_dvar
             if p=='rmax':
                 # rmax deriv
                 if params['shift']:
@@ -460,15 +462,17 @@ class PairPotential(abc.ABC):
                 below = np.zeros(r.shape[0], dtype=bool)
                 if params['rmin'] is not False:
                     below = r < params['rmin']
-                    deriv[below] += -self._force(params['rmin'], **params)
+                    deriv[below] += self._derivative(p, params['rmin'], **params)*dp_dvar
                 above = np.zeros(r.shape[0], dtype=bool)
                 if params['rmax'] is not False:
                     above = r > params['rmax']
-                    deriv[above] += -self._force(params['rmax'], **params)
+                    deriv[above] += self._derivative(p, params['rmax'], **params)*dp_dvar
+                elif params['shift']:
+                    raise ValueError('Cannot shift without setting rmax.')
                 flags = np.logical_and(~below, ~above)
                 deriv[flags] += self._derivative(p, r[flags], **params)*dp_dvar
                 if params['shift']:
-                    deriv[flags] -= -self._force(params['rmax'], **params)
+                    deriv -= self._derivative(p, params['rmax'], **params)*dp_dvar
 
         # coerce derivative back into shape of the input
         if scalar_r:

tests/potential/test_potential.py
@@ -593,7 +593,7 @@ class test_PairPotential(unittest.TestCase):
         d = p.derivative(pair=('1','1'), var=x, r=0.6)
         self.assertAlmostEqual(d, 0.6)
         d = p.derivative(pair=('1','1'), var=x, r=[0.25,0.75])
-        np.testing.assert_allclose(d, [2.0,0.75])
+        np.testing.assert_allclose(d, [0.5,0.75])
 
         #test with rmax set
         rmax = relentless.DesignVariable(value=1.5)
@@ -601,14 +601,14 @@ class test_PairPotential(unittest.TestCase):
         d = p.derivative(pair=('1','1'), var=x, r=1.0)
         self.assertAlmostEqual(d, 1.0)
         d = p.derivative(pair=('1','1'), var=x, r=[0.25,1.75])
-        np.testing.assert_allclose(d, [0.25,2.0])
+        np.testing.assert_allclose(d, [0.25,1.5])
 
         #test with rmin and rmax set
         p.coeff['1','1']['rmin'] = rmin
         d = p.derivative(pair=('1','1'), var=x, r=0.75)
         self.assertAlmostEqual(d, 0.75)
         d = p.derivative(pair=('1','1'), var=x, r=[0.25,0.5,1.5,1.75])
-        np.testing.assert_allclose(d, [2.0,0.5,1.5,2.0])
+        np.testing.assert_allclose(d, [0.5,0.5,1.5,1.5])
 
         #test w.r.t. rmin and rmax
         d = p.derivative(pair=('1','1'), var=rmin, r=[0.25,1.0,2.0])
@@ -619,9 +619,9 @@ class test_PairPotential(unittest.TestCase):
         #test parameter derivative with shift set
         p.coeff['1','1'].update(shift=True)
         d = p.derivative(pair=('1','1'), var=x, r=0.5)
-        self.assertAlmostEqual(d, -1.5)
+        self.assertAlmostEqual(d, -1.0)
         d = p.derivative(pair=('1','1'), var=x, r=[0.25,1.0,1.5,1.75])
-        np.testing.assert_allclose(d, [0.0,-1.0,-0.5,0.0])
+        np.testing.assert_allclose(d, [-1.0,-0.5,0.0,0.0])
 
         #test w.r.t. rmin and rmax, shift set
         d = p.derivative(pair=('1','1'), var=rmin, r=[0.25,1.0,2.0])