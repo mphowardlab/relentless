7e6a2d891f044124167f81edeb5f83128ca15efd
adisr04 <adithya.sr@utexas.edu>
2021-02-15 17:21:36 -0600

    Implement steepest descent iterator functionality

--- relentless/optimize/method.py
+++ relentless/optimize/method.py
@@ -9,11 +9,41 @@ class Optimizer(abc.ABC):
         pass
 
 class SteepestDescent(Optimizer):
-    def __init__(self, alpha):
+    def __init__(self, alpha, max_iter, abs_tol, mode):
         self.alpha = alpha
+        self.max_iter = max_iter
+        self.abs_tol = abs_tol
+        self.mode = mode
 
     def optimize(self, objective):
         dvars = objective.design_variables()
-        res = objective.compute()
-        for x in dvars:
-            x.value -= self.alpha*res.gradient(x)
+        iter_num = 1
+        update = {}
+
+        while True:
+            res = objective.compute()
+            for x in dvars:
+                update[x] = self.alpha*res.gradient(x)
+
+            if iter_num > self.max_iter:
+                raise RuntimeError('Could not find solution within ' + str(self.max_iter) + ' iterations.')
+            elif self._has_converged(update):
+                print('Solution found in ' + str(iter_num) + ' iterations.')
+                break
+            #TODO: add this branch?
+            #elif alpha*update/value
+                # raise ValueError('Converged to wrong minimum.')
+            else:
+                for x in dvars:
+                    x.value -= update[x]
+
+            iter_num += 1
+
+    def _has_converged(self, update):
+        if self.mode=='grad_diff':
+            for x in update:
+                if abs(update[x]) > (self.abs_tol[x] if isinstance(self.abs_tol, dict) else self.abs_tol):
+                    return False
+            return True
+        else:
+            raise ValueError('Convergence mode not found.')
--- tests/optimize/test_method.py
+++ tests/optimize/test_method.py
@@ -12,7 +12,22 @@ class test_SteepestDescent(unittest.TestCase):
         """Test run method."""
         x = relentless.variable.DesignVariable(value=3.0)
         q = QuadraticObjective(x=x)
-        o = relentless.optimize.SteepestDescent(alpha=0.25)
+        o = relentless.optimize.SteepestDescent(alpha=0.25, max_iter=1000, abs_tol=1e-8, mode='grad_diff')
 
         o.optimize(objective=q)
         self.assertAlmostEqual(x.value, 1.0)
+
+        x.value = -9.81
+        o.optimize(objective=q)
+        self.assertAlmostEqual(x.value, 1.0)
+
+        #test insufficient maximum iterations
+        x.value = 1.5
+        o = relentless.optimize.SteepestDescent(alpha=0.25, max_iter=1, abs_tol=1e-8, mode='grad_diff')
+        with self.assertRaises(RuntimeError):
+            o.optimize(objective=q)
+
+        #test invalid convergence mode
+        o = relentless.optimize.SteepestDescent(alpha=0.25, max_iter=1000, abs_tol=1e-8, mode='percent_error')
+        with self.assertRaises(ValueError):
+            o.optimize(objective=q)
