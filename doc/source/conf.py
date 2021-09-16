bca5814c64c0072b3bfd195ab779351e2500ec35
Michael Howard <mphoward@auburn.edu>
2021-09-16 16:29:22 -0500

    Fix CSS style override

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -40,11 +40,7 @@ exclude_patterns = []
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
-html_context = {
-    'css_files': [
-        '_static/theme_overrides.css',
-        ],
-    }
+html_css_files = ['theme_overrides.css']
 
 # -- Options for autodoc & autosummary ---------------------------------------
 
