af8a57b64d03994209c4582e6ef6fec28d29737f
clpetix <clpetix@gmail.com>
2022-07-12 12:28:38 -0500

    Added 2-D area analog of each 3-D volume object.

--- relentless/volume.py
+++ relentless/volume.py
@@ -1,7 +1,7 @@
 """
 Volumes
 =======
-A :class:`Volume` represents a 3D region of space with a fixed, scalar volume. It corresponds
+A :class:`Extent` represents a region of space with a fixed, scalar volume or area. It corresponds
 to the "box" used in simulations. The following box types have been implemented:
 
 .. autosummary::
@@ -11,6 +11,10 @@ to the "box" used in simulations. The following box types have been implemented:
     TriclinicBox
     Cuboid
     Cube
+    Parallelogram
+    ObliqueArea
+    Rectangle
+    Square
 
 The :class:`TriclinicBox` can be constructed using both the LAMMPS and HOOMD-blue
 :class:`TriclinicBox.Convention`\s for applying tilt factors.
@@ -39,7 +43,11 @@ and define the required methods.
 
     Volume
 
+.. autoclass:: Extent
+    :members:
 .. autoclass:: Volume
+    :members:
+.. autoclass:: Area
     :members:
 .. autoclass:: Parallelepiped
     :members:
@@ -48,6 +56,8 @@ and define the required methods.
 .. autoclass:: Cuboid
     :members:
 .. autoclass:: Cube
+    :members:
+.. autoclass:: Parallelogram
     :members:
 
 """
@@ -59,7 +69,7 @@ import numpy
 class Extent(abc.ABC):
     r"""Abstract base class defining a region of space.
 
-    A Volume can be any region of space; typically, it is a simulation "box."
+    An Extent can be any region of space; typically, it is a simulation "box."
     Any deriving class must implement the volume method that computes the scalar
     volume of the region. Additionally, methods to serialize and deserialize a
     Volume must be specified so that the object can be saved to disk.
@@ -94,7 +104,7 @@ class Extent(abc.ABC):
         pass
 
 class Volume(Extent):
-    r"""Abstract base class defining a 3d region of space.
+    r"""Abstract derived class defining a 3d region of space.
 
     A Volume can be any region of space; typically, it is a simulation "box."
     Any deriving class must implement the volume method that computes the scalar
@@ -134,7 +144,7 @@ class Volume(Extent):
         pass
 
 class Area(Extent):
-    r"""Abstract base class defining a 2d region of space.
+    r"""Abstract derived class defining a 2d region of space.
 
     An Area can be any 2d region of space; typically, it is a simulation "box."
     Any deriving class must implement the volume method that computes the scalar
@@ -156,7 +166,7 @@ class Area(Extent):
         Returns
         -------
         dict
-            The serialized :class:`Volume` data.
+            The serialized :class:`Area` data.
 
         """
         pass
@@ -167,7 +177,7 @@ class Area(Extent):
 
         Returns
         -------
-        :class:`Volume`
+        :class:`Area`
             The object reconstructed from the ``data``.
 
         """
@@ -506,30 +516,319 @@ class Cube(Cuboid):
         """
         return Cube(**data)
 
-class Parallelegram(Area):
-        r"""Parallelepiped box defined by three vectors.
+class Parallelogram(Area):
+    r"""Parallelogram box defined by two vectors.
 
-    The three vectors :math:`\mathbf{a}`, :math:`\mathbf{b}`, and :math:`\mathbf{c}`
-    must form a right-hand basis so that the box volume :math:`V` is positive:
+    The two vectors :math:`\mathbf{a}` and :math:`\mathbf{b}`
+    must form a right-hand basis so that the box volume :math:`A` is positive:
 
     .. math::
 
-        V = (\mathbf{a} \times \mathbf{b}) \cdot \mathbf{c} > 0
+        A = |(\mathbf{a} \times \mathbf{b})| > 0
 
     Parameters
     ----------
     a : array_like
-        First vector defining the parallelepiped.
+        First vector defining the parallelogram.
     b : array_like
-        Second vector defining the parallelepiped.
-    c : array_like
-        Third vector defining the parallelepiped.
+        Second vector defining the parallelogram.
 
     Raises
     ------
     TypeError
-        If ``a``, ``b``, and ``c`` are not all 3-element vectors.
+        If ``a`` and ``b`` are not all 3-element vectors.
     ValueError
-        If the volume is not positive.
+        If the area is not positive.
 
     """
+    def __init__(self, a, b):
+        self.a = numpy.asarray(a,dtype=numpy.float64)
+        self.b = numpy.asarray(b,dtype=numpy.float64)
+        if not (self.a.shape==(3,) and self.b.shape==(3,)):
+            raise TypeError('a and b must be 3-element vectors.')
+        if self.area <= 0:
+            raise ValueError('The volume must be positive.')
+
+    @property
+    def area(self):
+        return numpy.linalg.norm(numpy.cross(self.a,self.b))
+
+    def to_json(self):
+        r"""Serialize as a dictionary.
+
+        The dictionary contains the three box vectors ``a`` and ``b`` as tuples.
+
+        Returns
+        -------
+        dict
+            The serialized Parallelogram.
+
+        """
+        return {'a': tuple(self.a),
+                'b': tuple(self.b)
+               }
+
+    @classmethod
+    def from_json(cls, data):
+        r"""Deserialize from a dictionary.
+
+        Parameters
+        ----------
+        data : dict
+            The serialized equivalent of the Parallelogram object. The keys
+            of ``data`` should be ``('a','b')``, and the data for each
+            is the 3-element box vector.
+
+        Returns
+        -------
+        :class:`Parallelogram`
+            A new Parallelogram object constructed from the data.
+
+        """
+        return Parallelepiped(**data)
+
+class ObliqueArea(Parallelogram):
+    r"""Oblique area.
+
+    A ObliqueArea is a special type of :class:`Parallelogram`. The box is
+    defined by an area oriented along the Cartesian axes and having
+    three vectors of length :math:`L_x` and :math:`L_y`, respectively.
+    The box is then tilted by factors :math:`xy`, which
+    is upper off-diagonal elements of the matrix of box vectors. As a result,
+    the :math:`\mathbf{a}` vector is always aligned along the :math:`x` axis, while
+    the other vectors may be tilted.
+
+    The tilt factors can be defined using one of two :class:`ObliqueArea.Convention`\s.
+    By default, the LAMMPS convention is applied to calculate the basis vectors.
+
+    Parameters
+    ----------
+    Lx : float
+        Length along the :math:`x` axis.
+    Ly : float
+        Length along the :math:`y` axis.
+    xy : float
+        Tilt factor.
+
+
+    Raises
+    ------
+    ValueError
+        If ``Lx`` and ``Ly`` are not both positive.
+    ValueError
+        If the convention is not ``ObliqueArea.Convention.LAMMPS`` or
+        ``ObliqueArea.Convention.HOOMD``.
+
+    """
+    
+    class Convention(Enum):
+        r"""Convention by which the tilt factors are applied to the basis vectors.
+
+        In the `LAMMPS <https://lammps.sandia.gov/doc/Howto_triclinic.html>`_
+        simulation convention, specified using ``TriclinicBox.Convention.LAMMPS``,
+        the basis vectors are
+
+        .. math::
+
+            \mathbf{a} = (L_x,0,0)
+            \quad \mathbf{b} = (xy,L_y,0)
+            \quad \mathbf{c} = (xz,yz,L_z)
+
+        In the `HOOMD-blue <https://hoomd-blue.readthedocs.io/en/stable/box.html>`_
+        simulation convention, specified using ``TriclinicBox.Convention.HOOMD``,
+        the basis vectors are
+
+        .. math::
+
+            \mathbf{a} = (L_x,0,0)
+            \quad \mathbf{b} = (xy \cdot L_y,L_y,0)
+            \quad \mathbf{c} = (xz \cdot L_z,yz \cdot L_z,L_z)
+
+        Attributes
+        ----------
+        LAMMPS : int
+            LAMMPS convention for applying the tilt factors.
+        HOOMD : int
+            HOOMD convention for applying the tilt factors.
+
+        """
+        LAMMPS = 1
+        HOOMD = 2
+
+    def __init__(self, Lx, Ly, xy, convention=Convention.LAMMPS):
+        Lz = 0
+        xz = 0
+        yz = 0
+        if Lx<=0 or Ly<=0:
+            raise ValueError('All side lengths must be positive.')
+        self._convention = convention
+        if self.convention is ObliqueArea.Convention.LAMMPS:
+            a = (Lx,0,0)
+            b = (xy,Ly,0)
+            c = (xz,yz,Lz)
+        elif self.convention is ObliqueArea.Convention.HOOMD:
+            a = (Lx,0,0)
+            b = (xy*Ly,Ly,0)
+            c = (xz*Lz,yz*Lz,Lz)
+        else:
+            raise ValueError('Triclinic convention must be ObliqueArea.Convention.LAMMPS or ObliqueArea.Convention.HOOMD')
+        super().__init__(a,b,c)
+
+    @property
+    def convention(self):
+        r""":class:`ObliqueArea.Convention`: Convention for tilt factors."""
+        return self._convention
+
+    def to_json(self):
+        r"""Serialize as a dictionary.
+
+        The dictionary contains the three box lengths ``Lx`` and ``Ly``,
+        the tilt factors ``xy``, and the ``convention``
+        for the tilt factors.
+
+        Returns
+        -------
+        dict
+            The serialized ObliqueArea.
+
+        """
+        if self._convention is ObliqueArea.Convention.LAMMPS:
+            xy = self.b[0]
+        elif self._convention is ObliqueArea.Convention.HOOMD:
+            xy = self.b[0]/self.b[1]
+        return {'Lx': self.a[0],
+                'Ly': self.b[1],
+                'xy': xy,
+                'convention': self._convention.name
+               }
+
+    @classmethod
+    def from_json(cls, data):
+        r"""Deserialize from a dictionary.
+
+        Parameters
+        ----------
+        data : dict
+            The serialized equivalent of the ObliqueArea object. The keys
+            of ``data`` should be ``('Lx','Ly','Lz','xy','xz','yz','convention')``.
+            The lengths and tilt factors should be floats, and the convention should
+            be a string.
+
+        Returns
+        -------
+        :class:`ObliqueArea`
+            A new ObliqueArea object constructed from the data.
+
+        Raises
+        ------
+        ValueError
+            If the convention specified is not ``'LAMMPS'`` or ``'HOOMD'``.
+
+        """
+        data_ = dict(data)
+        if data['convention']=='LAMMPS':
+            data_['convention'] = ObliqueArea.Convention.LAMMPS
+        elif data['convention']=='HOOMD':
+            data_['convention'] = ObliqueArea.Convention.HOOMD
+        else:
+            return ValueError('Only LAMMPS and HOOMD conventions are supported.')
+        return ObliqueArea(**data_)
+
+class Rectangle(ObliqueArea):
+    r"""Rectangle box.
+
+    A Reactangle is a special type of :class:`ObliqueArea`. The two box vectors
+    point along the :math:`x` and :math:`y`, so they are all
+    orthogonal (i.e. :math:`xy=0`). Each vector can have a different length,
+    :math:`L_x` and :math:`L_y`.
+
+    Parameters
+    ----------
+    Lx : float
+        Length along the :math:`x` axis.
+    Ly : float
+        Length along the :math:`y` axis..
+
+    """
+    def __init__(self, Lx, Ly):
+        super().__init__(Lx,Ly,0)
+
+    def to_json(self):
+        r"""Serialize as a dictionary.
+
+        The dictionary contains the two box lengths ``Lx`` and ``Ly``.
+
+        Returns
+        -------
+        dict
+            The serialized Rectangle.
+
+        """
+        return {'Lx': self.a[0],
+                'Ly': self.b[1],
+               }
+
+    @classmethod
+    def from_json(cls, data):
+        r"""Deserialize from a dictionary.
+
+        Parameters
+        ----------
+        data : dict
+            The serialized equivalent of the Rectangle object. The keys
+            of ``data`` should be ``('Lx','Ly')``, and their values
+            should be floats.
+
+        Returns
+        -------
+        :class:`Rectangle`
+            A new Rectangle object constructed from the data.
+
+        """
+        return Rectangle(**data)
+
+class Square(Rectangle):
+    r"""Square box.
+
+    A Square is a special type of :class:`Rectangle` where all vectors have the
+    same length :math:`L`.
+
+    Parameters
+    ----------
+    L : float
+        The edge length of the square.
+
+    """
+    def __init__(self, L):
+        super().__init__(L,L)
+
+    def to_json(self):
+        r"""Serialize as a dictionary.
+
+        The dictionary contains the box length ``L``.
+
+        Returns
+        -------
+        dict
+            The serialized Square.
+
+        """
+        return {'L': self.a[0]}
+
+    @classmethod
+    def from_json(cls, data):
+        r"""Deserialize from a dictionary.
+
+        Parameters
+        ----------
+        data : dict
+            The serialized equivalent of the Square object. The keys
+            of ``data`` should be ``('L',)``, and its value should be a float.
+
+        Returns
+        -------
+        :class:`Square`
+            A new Square object constructed from the data.
+
+        """
+        return Square(**data)
