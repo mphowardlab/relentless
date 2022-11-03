2f8e7ef9bbaca167fd59a25ae53ced2f27f330e7
Michael Howard <mphoward@auburn.edu>
2022-11-02 21:03:13 -0500

    Also fix LJ potential ref

--- relentless/simulate/hoomd.py
+++ relentless/simulate/hoomd.py
@@ -749,7 +749,7 @@ class HOOMD(simulate.Simulation):
     .. warning::
 
         HOOMD requires that tabulated pair potentials be finite. A common place to have an
-        infinite value is at :math:`r=0`, since potentials like :class:`~relentless.potential.LennardJones`
+        infinite value is at :math:`r=0`, since potentials like :class:`~relentless.potential.pair.LennardJones`
         diverge there. You should make sure to exclude these values from the tabulated potentials,
         e.g., setting :attr:`~relentless.simulate.PairPotentialTabulator.rmin` to a small value larger than 0.
 
