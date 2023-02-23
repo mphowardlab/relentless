c8f87730bc7eb4ab2de32fd690a398f6986f57fe
clpetix <clpetix@gmail.com>
2023-02-23 16:50:57 -0600

    Pack in order of descending diameters.

--- src/relentless/simulate/initialize.py
+++ src/relentless/simulate/initialize.py
@@ -1,3 +1,4 @@
+import copy
 import itertools
 
 import numpy
@@ -123,11 +124,16 @@ class InitializeRandomly(simulate.DelegatedInitializationOperation):
         types = []
         trees = {}
         Nadded = 0
+        evaluated_diameters = copy.deepcopy(diameters)
+        for keys in evaluated_diameters:
+            evaluated_diameters[keys] = variable.evaluate(evaluated_diameters[keys])
         # insert the particles, big to small
-        sorted_N = sorted(N.items(), key=lambda x: x[1], reverse=True)
-        for i, Ni in sorted_N:
+        sorted_diameters = sorted(
+            evaluated_diameters.items(), key=lambda x: x[1], reverse=True
+        )
+        for i, di in sorted_diameters:
             # generate site coordinates, on orthorhombic lattices
-            di = variable.evaluate(diameters[i])
+            Ni = N[i]
             if dimension == 3:
                 # fcc lattice
                 a = numpy.sqrt(2.0) * di
@@ -165,7 +171,7 @@ class InitializeRandomly(simulate.DelegatedInitializationOperation):
             if len(trees) > 0:
                 mask = numpy.ones(sites.shape[0], dtype=bool)
                 for j, treej in trees.items():
-                    dj = diameters[j]
+                    dj = variable.evaluate(diameters[j])
                     num_overlap = treej.query_ball_point(
                         sites, 0.5 * (di + dj), return_length=True
                     )
