7fbecd5893e2feb731370e93ec6de6e3c1f15193
Michael Howard <mphoward@auburn.edu>
2024-04-28 22:36:44 -0500

    Histogram and normalize RDFs manually

--- src/relentless/simulate/hoomd.py
+++ src/relentless/simulate/hoomd.py
@@ -1200,12 +1200,16 @@ class EnsembleAverage(AnalysisOperation):
                     # compute number of particles of each type
                     # save type masks for use in RDF calculations if needed
                     type_masks = {}
+                    N = {}
                     for i in self.types:
                         type_masks[
                             i
                         ] = snap.particles.typeid == snap.particles.types.index(i)
                         if "N" not in self.constraints:
-                            self._N[i] += numpy.sum(type_masks[i])
+                            N[i] = numpy.sum(type_masks[i])
+                            self._N[i] += N[i]
+                        else:
+                            N[i] = self.constraints["N"][i]
 
                     # then do rdf if requested
                     if compute_rdf:
@@ -1233,6 +1237,10 @@ class EnsembleAverage(AnalysisOperation):
                             )
                         box = freud.box.Box.from_box(box_array, dimensions=dimensions)
 
+                        # calculate average density per type
+                        for i in self.types:
+                            self._rdf_density[i] += N[i] / box.volume
+
                         # build aabb of all particles and generate a parent
                         # neighbor list with the RDF cutoff
                         aabb = freud.locality.AABBQuery(box, snap.particles.position)
@@ -1259,34 +1267,19 @@ class EnsembleAverage(AnalysisOperation):
                             )
                             neighbors.filter(bond_exclusion_filter)
 
-                        for i, j in self._rdf:
-                            # make a neighbor list of all particles of type i
-                            # whose neighbors are type j
-                            neighbors_ij = neighbors.copy()
-                            filter_ij = numpy.logical_and(
-                                type_masks[i][neighbors[:, 0]],
-                                type_masks[j][neighbors[:, 1]],
-                            )
-                            # if the types are different, also consider
-                            # neighbors of j that are type i
-                            if j != i:
-                                filter_ji = numpy.logical_and(
-                                    type_masks[j][neighbors[:, 0]],
-                                    type_masks[i][neighbors[:, 1]],
+                        for i in self.types:
+                            for j in self.types:
+                                filter_ij = numpy.logical_and(
+                                    type_masks[i][neighbors[:, 0]],
+                                    type_masks[j][neighbors[:, 1]],
                                 )
-                                filter_ij = numpy.logical_or(filter_ij, filter_ji)
-                            neighbors_ij.filter(filter_ij)
+                                counts, _ = numpy.histogram(
+                                    neighbors.distances[filter_ij],
+                                    bins=self.rdf_params["bins"],
+                                    range=(0, self.rdf_params["stop"]),
+                                )
+                                self._rdf_counts[i, j] += counts
 
-                            # it doesn't look like it but this calculation should
-                            # only calculate g_ij because we prefiltered the neighbors
-                            #
-                            # resetting when the samples are zero clears the RDF
-                            self._rdf[i, j].compute(
-                                aabb,
-                                snap.particles.position,
-                                neighbors=neighbors_ij,
-                                reset=(self.num_samples == 0),
-                            )
             self.num_samples += 1
             if compute_rdf:
                 self.num_rdf_samples += 1
@@ -1312,15 +1305,20 @@ class EnsembleAverage(AnalysisOperation):
                 self._N[i] = 0
 
             if self.rdf_params is not None:
-                self._rdf = collections.PairMatrix(self.types)
-                for i, j in self._rdf:
-                    self._rdf[i, j] = freud.density.RDF(
-                        bins=self.rdf_params["bins"],
-                        r_max=self.rdf_params["stop"],
-                        normalize=(i == j),
-                    )
+                self._rdf_counts = {}
+                self._rdf_density = {}
+                self._rdf_num_origins = {}
+                for i in self.types:
+                    self._rdf_density[i] = 0
+                    self._rdf_num_origins[i] = 0
+                    for j in self.types:
+                        self._rdf_counts[i, j] = numpy.zeros(
+                            self.rdf_params["bins"], dtype=int
+                        )
             else:
-                self._rdf = None
+                self._rdf_counts = None
+                self._rdf_density = None
+                self._rdf_num_origins = None
 
             self.num_samples = 0
             self.num_rdf_samples = 0
@@ -1389,16 +1387,49 @@ class EnsembleAverage(AnalysisOperation):
             Radial distribution functions.
             """
             if self.num_rdf_samples > 0:
+                bin_edges = numpy.linspace(
+                    0, self.rdf_params["stop"], self.rdf_params["bins"] + 1
+                )
+                bin_centers = 0.5 * (bin_edges[:-1] + bin_edges[1:])
+                if self.dimension == 3:
+                    bin_extents = (4 * numpy.pi / 3) * (
+                        bin_edges[1:] ** 3 - bin_edges[:-1] ** 3
+                    )
+                elif self.dimension == 2:
+                    bin_extents = numpy.pi * (bin_edges[1:] ** 2 - bin_edges[:-1] ** 2)
+
                 rdf = collections.PairMatrix(self.types)
-                for pair in rdf:
+                for i, j in rdf:
                     if mpi.world.rank == 0:
-                        gr = numpy.column_stack(
-                            (self._rdf[pair].bin_centers, self._rdf[pair].rdf)
-                        )
+                        density = {
+                            k: self._rdf_density[k] / self.num_rdf_samples
+                            for k in self.types
+                        }
+                        g = numpy.zeros_like(bin_centers)
+                        if i == j:
+                            if self._rdf_num_origins[i] > 0 and density[i] > 0:
+                                g = self._rdf_counts[i, i] / (
+                                    self._rdf_num_origins[i] * density[i] * bin_extents
+                                )
+                        else:
+                            # this takes the weighted average of g_ij and g_ji
+                            num_ij_origins = (
+                                self._rdf_num_origins[i] + self._rdf_num_origins[j]
+                            )
+                            if num_ij_origins > 0:
+                                if density[j] > 0:
+                                    g += self._rdf_counts[i, j] / (
+                                        num_ij_origins * density[j] * bin_extents
+                                    )
+                                if density[i] > 0:
+                                    g += self._rdf_counts[j, i] / (
+                                        num_ij_origins * density[i] * bin_extents
+                                    )
+                        gr = numpy.column_stack((bin_centers, g))
                     else:
                         gr = None
                     gr = mpi.world.bcast_numpy(gr, root=0)
-                    rdf[pair] = ensemble.RDF(gr[:, 0], gr[:, 1])
+                    rdf[i, j] = ensemble.RDF(gr[:, 0], gr[:, 1])
                 return rdf
             else:
                 return None
