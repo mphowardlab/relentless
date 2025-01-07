639b4acb2841cc0a60a254089d44e522ffcc86a6
clpetix <clpetix@gmail.com>
2025-01-07 16:26:07 -0600

    Fix angle derivative test.

--- tests/model/potential/test_angle.py
+++ tests/model/potential/test_angle.py
@@ -121,9 +121,9 @@ class test_AnglePotential(unittest.TestCase):
         p.coeff["1"]["m"] = x
 
         # test with no cutoffs
-        d = p.derivative(types=("1"), var=x, r=0.5)
+        d = p.derivative(type_=("1"), var=x, r=0.5)
         self.assertAlmostEqual(d, 0.5)
-        d = p.derivative(types=("1"), var=x, r=[0.25, 0.75])
+        d = p.derivative(type_=("1"), var=x, r=[0.25, 0.75])
         numpy.testing.assert_allclose(d, [0.25, 0.75])
 
     def test_derivative_types(self):
@@ -135,20 +135,20 @@ class test_AnglePotential(unittest.TestCase):
         q.coeff["1"]["m"] = z
 
         # test with respect to dependent variable parameter
-        d = q.derivative(types=("1"), var=z, r=2.0)
+        d = q.derivative(type_=("1"), var=z, r=2.0)
         self.assertAlmostEqual(d, 2.0)
 
         # test with respect to independent variable on which parameter is dependent
-        d = q.derivative(types=("1"), var=x, r=1.5)
+        d = q.derivative(type_=("1"), var=x, r=1.5)
         self.assertAlmostEqual(d, 3.0)
-        d = q.derivative(types=("1"), var=y, r=4.0)
+        d = q.derivative(type_=("1"), var=y, r=4.0)
         self.assertAlmostEqual(d, 0.5)
 
         # test invalid derivative w.r.t. scalar
         a = 2.5
         q.coeff["1"]["m"] = a
         with self.assertRaises(TypeError):
-            d = q.derivative(types=("1"), var=a, r=2.0)
+            d = q.derivative(type_=("1"), var=a, r=2.0)
 
         # test with respect to independent variable which is
         # related to a SameAs variable
@@ -156,12 +156,12 @@ class test_AnglePotential(unittest.TestCase):
 
         r.coeff["1"]["x"] = x
         r.coeff["1"]["y"] = relentless.model.variable.SameAs(x)
-        d = r.derivative(types=("1"), var=x, r=4.0)
+        d = r.derivative(type_=("1"), var=x, r=4.0)
         self.assertAlmostEqual(d, 20.0)
 
         r.coeff["1"]["y"] = x
         r.coeff["1"]["x"] = relentless.model.variable.SameAs(x)
-        d = r.derivative(types=("1"), var=x, r=4.0)
+        d = r.derivative(type_=("1"), var=x, r=4.0)
         self.assertAlmostEqual(d, 20.0)
 
     def test_iteration(self):
