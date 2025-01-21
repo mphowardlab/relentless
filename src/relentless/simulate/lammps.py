1e5b843457b3d03fbf2c9c8319758f6f1f11386e
Levi Petix <81758680+clpetix@users.noreply.github.com>
2025-01-21 16:33:45 -0600

    Add support for LAMMPS 2024.08.29, drop support for LAMMPS 2022.06.23 (#290)
    * Run tests using conda-selected MPI library.
    
    * Force conda to use MPICH
    
    * Drop support for 2022.06.023.
    
    * Exclude Python 3.12 and LAMMPS 2023.08.02.
    
    * Add support for 2024.08.29.
    
    * Update doc string and bump version check.
    
    * Remove 2022.06.23 check on RunBrownianDynamics.

--- src/relentless/simulate/lammps.py
+++ src/relentless/simulate/lammps.py
@@ -863,11 +863,6 @@ class RunBrownianDynamics(_Integrator):
     def _call_commands(self, sim):
         if "BROWNIAN" not in sim["engine"]["packages"]:
             raise NotImplementedError("LAMMPS BROWNIAN package is not installed.")
-        elif sim["engine"]["version"] < 20220623:
-            raise NotImplementedError(
-                "LAMMPS versions prior to 23Jun2022 stable release do not"
-                " properly support Brownian dynamics."
-            )
 
         T = self._make_T(self.T)
         if T[0] != T[1]:
@@ -1770,7 +1765,7 @@ class LAMMPS(simulate.Simulation):
     GPUs, as a single process or with MPI parallelism. The launch configuration
     will be automatically selected for you when the simulation is run.
 
-    The version of LAMMPS must be 29 Sep 2021 or newer. It is recommended to build
+    The version of LAMMPS must be 02 Aug 2023 or newer. It is recommended to build
     LAMMPS with its `Python interface <https://docs.lammps.org/Python_head.html>`_.
     However, it is possible to run LAMMPS as a binary by specifying ``executable``::
 
@@ -1880,8 +1875,8 @@ class LAMMPS(simulate.Simulation):
             self.version = lmp.version()
             self.packages = tuple(lmp.installed_packages)
             del lmp
-        if self.version < 20210929:
-            raise ImportError("Only LAMMPS 29 Sep 2021 or newer is supported.")
+        if self.version < 20230802:
+            raise ImportError("Only LAMMPS 02 Aug 2023 or newer is supported.")
 
         super().__init__(initializer, operations)
         self.quiet = quiet
