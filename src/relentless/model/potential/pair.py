209f9a9a7f2d2d1758639a8c73483beb1479405c
clpetix <clpetix@gmail.com>
2025-03-20 14:05:59 -0500

    Refactor to not redudantly pass coordinate back when validating in pair too.

--- src/relentless/model/potential/pair.py
+++ src/relentless/model/potential/pair.py
@@ -220,7 +220,7 @@ class PairPotential(potential.Potential):
         """
         params = self.coeff.evaluate(pair)
         r, u, scalar_r = self._zeros(r)
-        r = self._validate_coordinate(r)
+        self._validate_coordinate(r)
 
         # evaluate at points below rmax (if set) first, including rmin cutoff (if set)
         flags = numpy.ones(r.shape[0], dtype=bool)
@@ -275,7 +275,7 @@ class PairPotential(potential.Potential):
         """
         params = self.coeff.evaluate(pair)
         r, f, scalar_r = self._zeros(r)
-        r = self._validate_coordinate(r)
+        self._validate_coordinate(r)
 
         # only evaluate at points inside [rmin,rmax], if specified
         flags = numpy.ones(r.shape[0], dtype=bool)
@@ -329,7 +329,7 @@ class PairPotential(potential.Potential):
         """
         params = self.coeff.evaluate(pair)
         r, deriv, scalar_r = self._zeros(r)
-        r = self._validate_coordinate(r)
+        self._validate_coordinate(r)
         if not isinstance(var, variable.Variable):
             raise TypeError(
                 "Parameter with respect to which to take the derivative"
@@ -475,7 +475,6 @@ class PairPotential(potential.Potential):
         """Validate the coordinate for pair potentials."""
         if numpy.any(r < 0):
             raise ValueError("Pair distance must be non-negative")
-        return r
 
 
 class Depletion(PairPotential):
